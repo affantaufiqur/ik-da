4ca8fa14a636a8d6218cf5fe7321a28785d5d765
Affan <affantaufiqur@gmail.com>
2024-02-13 20:35:37 +0700

    left

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json("Hello World!");
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
