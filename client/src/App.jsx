da71197d812b73cb1fdb5a49513fd7899aaac3f9
Affan <affantaufiqur@gmail.com>
2024-02-18 14:19:19 +0700

    fix react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,8 @@
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Tes />} />
-      </Route>
-    </Routes>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      {" akmklal "}
-      <Outlet />
-    </div>
+    <p>
+      div <Outlet />
+    </p>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { BrowserRoute as Router, Routes, Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [],
-  },
-]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
