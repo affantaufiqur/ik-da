cb11fdd58059679fedb74974135984b62fab767a
AzizRahmawan <arder058@gmail.com>
2024-02-19 13:18:54 +0700

    update role and add user seed

--- server/seeder/role-seed.js
+++ server/seeder/role-seed.js
@@ -1,14 +1,14 @@
 import { uuidToULID } from "ulidx";
 import prisma from "../app/prisma.js";
 
+export const Role = {
+    ADMIN: 'admin', 
+    USER: 'user'
+}
 async function main() {
     await prisma.user.deleteMany();
     await prisma.role.deleteMany();
 
-    const Role = {
-        ADMIN: 'admin', 
-        USER: 'user'
-    }
     for (const role in Role) {
         await prisma.role.create({
             data: {
--- server/seeder/user-seed.js
+++ server/seeder/user-seed.js
@@ -1,10 +1,24 @@
 import prisma from "../app/prisma.js";
+import { faker } from "@faker-js/faker";
+import { Role } from "./role-seed.js";
 
 const bcryptRound = Number(process.env.BCRYPT_ROUND);
 
 async function main() {
+    await prisma.story.deleteMany();
     await prisma.user.deleteMany();
+    const roles = await prisma.role.findMany();
     
+    for (let i = 1; i <= 5; i++) {
+        await prisma.user.create({
+            data: {
+                name: faker.person.fullName(),
+                email: faker.internet.email(),
+                password: `password ${i}`,
+                role_id: roles[Math.floor(Math.random() * roles.length)].id
+            }
+        });
+    }
 }
 
 main()
