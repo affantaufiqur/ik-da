dab584c692c4a8b5f1e088ed13bf86d7fe72bd2d
Amirul <muhamirul98@gmail.com>
2024-02-19 13:24:24 +0700

    install font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { BrowserRoute as Router, Routes, Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import * as Sentry from "@sentry/react";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [],
-  },
-]);
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", "127.0.0.1"],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  /*eslint no-undef: "off"*/
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "affan-q3",
+        project: "javascript-react",
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+
+    build: {
+      sourcemap: true,
+    },
+  };
+});
+
