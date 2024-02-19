8eaa678736dd842541950857f7ab63ed54c58a08
AzizRahmawan <arder058@gmail.com>
2024-02-19 10:15:59 +0700

    update params.id in chapter routes

--- server/app/routes/chapter-routes.js
+++ server/app/routes/chapter-routes.js
@@ -2,23 +2,23 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get('/stories/:id/chapters', async (req, res) => {
+routes.get('/stories/:storyId/chapters', async (req, res) => {
     res.json({ message: 'Route get stories chapters'});
 });
 
-routes.get('/stories/:id/chapters/:id', async (req, res) => {
+routes.get('/stories/:storyId/chapters/:chapterId', async (req, res) => {
     res.json({ message: 'Route get detail stories chapters'});
 });
 
-routes.post('/stories/:id/chapters', async (req, res) => {
+routes.post('/stories/:storyId/chapters', async (req, res) => {
     res.json({ message: 'Route create stories chapters'});
 });
 
-routes.put('/stories/:id/chapters/:id', async (req, res) => {
+routes.put('/stories/:storyId/chapters/:chapterId', async (req, res) => {
     res.json({ message: 'Route update stories chapters'});
 });
 
-routes.delete('/stories/:id/chapters/:id', async (req, res) => {
+routes.delete('/stories/:storyId/chapters/:chapterId', async (req, res) => {
     res.json({ message: 'Route delete stories chapters'});
 });
 
