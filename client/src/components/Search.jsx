f045f601b3d380f98909cb9aa57d55d22b260e25
Amirul <muhamirul98@gmail.com>
2024-02-19 14:40:17 +0700

    searchcategory

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,7 @@
 export default function Search() {
   return (
-    <div className="mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 rounded-lg p-2 justify-center">
-      <div className="w-1/3">
-        <label for="hero-input" className="sr-only">
-          Search something to read ...
-        </label>
-      </div>
+    <div className="w-full mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 rounded-lg p-2">
+      <input type="text" className="border-2 w-full" placeholder="Search something to read" />
     </div>
   );
 }
