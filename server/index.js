3c651f8452776bdfa7938bb9d88efcaaa8c8603d
Abdul Aziz Rahmawan <71802858+AzizRahmawan@users.noreply.github.com>
2024-02-19 11:57:39 +0700

    [BE]: Declare Routes (#10)
    * create model prisma database
    
    * update model prisma database
    
    * add auth routes
    
    * add user routes
    
    * add story routes
    
    * add chapter routes
    
    * update params.id in chapter routes
    
    * update params.id in story routes
    
    * update params.id in story routes
    
    * add genre routes
    
    * add bookmarks routes
    
    * add like book routes
    
    * import routes to index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import authRoutes from "./app/routes/auth-routes.js";
+import userRoutes from "./app/routes/user-routes.js";
+import storyRoutes from "./app/routes/story-routes.js";
+import chapterRoutes from "./app/routes/chapter-routes.js";
+import genreRoutes from "./app/routes/genre-routes.js";
+import bookmarkRoutes from "./app/routes/bookmark-routes.js";
+import booklikeRoutes from "./app/routes/booklike-routes.js";
 
 const app = express();
 const port = 3000;
@@ -8,4 +15,13 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.json("Hello World!");
 });
+
+app.use(authRoutes);
+app.use(userRoutes);
+app.use(storyRoutes); 
+app.use(chapterRoutes);
+app.use(genreRoutes);
+app.use(bookmarkRoutes);
+app.use(booklikeRoutes);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
