85616543d2822dd56353b835e0d581be6d461789
Amirul <muhamirul98@gmail.com>
2024-02-19 14:16:36 +0700

    search category

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,11 @@
+export default function Search() {
+  return (
+    <div className="mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 rounded-lg p-2 justify-center">
+      <div className="w-1/3">
+        <label for="hero-input" className="sr-only">
+          Search something to read ...
+        </label>
+      </div>
+    </div>
+  );
+}
