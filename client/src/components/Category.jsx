76239d699a6883b55cdd7b2c5ab48f7d64f0387e
MhammadAmirul <152957421+MhammadAmirul@users.noreply.github.com>
2024-02-19 14:50:25 +0700

    [FE]: search&category (#15)
    * install library
    
    * install lib
    
    * setting route
    
    * install font
    
    * search category
    
    * searchcategory

---
+++ client/src/components/Category.jsx
@@ -0,0 +1,12 @@
+export default function Category() {
+  return (
+    <div className="flex space-x-4">
+      <button type="button" className="py-2 px-6 inline-flex items-center gap-x-2 text-sm font-semibold border-2">
+        Action
+      </button>
+      <button type="button" className="py-2 px-6 inline-flex items-center gap-x-2 text-sm font-semibold border-2">
+        Fantasy
+      </button>
+    </div>
+  );
+}
