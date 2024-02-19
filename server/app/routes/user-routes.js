2d7326e039bf2e704ca4c76cc5f170614fdbc554
AzizRahmawan <arder058@gmail.com>
2024-02-19 10:17:33 +0700

    update params.id in story routes

--- server/app/routes/user-routes.js
+++ server/app/routes/user-routes.js
@@ -2,23 +2,23 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get('/user/:id', async (req, res) => {
+routes.get('/user/:userId', async (req, res) => {
     res.json({ message: 'Route get user profile'});
 });
 
-routes.put('/user/:id', async (req, res) => {
+routes.put('/user/:userId', async (req, res) => {
     res.json({ message: 'Route update user profile'});
 });
 
-routes.get('/user/:id/stories', async (req, res) => {
+routes.get('/user/:userId/stories', async (req, res) => {
     res.json({ message: 'Route get user stories'});
 });
 
-routes.get('/user/:id/bookmarks', async (req, res) => {
+routes.get('/user/:userId/bookmarks', async (req, res) => {
     res.json({ message: 'Route get user bookmarks'});
 });
 
-routes.get('/user/:id/book-likes', async (req, res) => {
+routes.get('/user/:userId/book-likes', async (req, res) => {
     res.json({ message: 'Route get user book likes'});
 });
 
