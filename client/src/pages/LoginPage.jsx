7cfdd70cfa76fea37534d4e7904499236ebcdbf5
affantaufiqur <affantaufiqur@gmail.com>
2024-02-20 16:25:12 +0700

    Login ui and fn

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,19 @@
+import PropTypes from "prop-types";
 import { yupResolver } from "mantine-form-yup-resolver";
 import { useForm } from "@mantine/form";
 import * as yup from "yup";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { Loader } from "lucide-react";
 
 const schemaPage = yup.object().shape({
-  email: yup.string().email("Invalid email").required("Required"),
-  password: yup.string().min(2, "Too short").required("Required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(2, "Too short").required("Password minimum is 6 characters"),
 });
 
 const LoginPage = () => {
+  const [status, setStatus] = useState(null);
+  const [submit, isSubmit] = useState(false);
   const form = useForm({
     initialValues: {
       email: "",
@@ -23,25 +28,37 @@ const LoginPage = () => {
       const res = await fetch("http://localhost:3000/login", {
         method: "POST",
         body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
       const data = await res.json();
-      console.log(data);
+      console.log(res);
+      if (res.status !== 200) {
+        setStatus(data.message);
+        isSubmit(false);
+        return;
+      }
+      document.cookie = `token=${data.token}`;
+      isSubmit(false);
       return data;
     },
   });
   const fields = Object.keys(schemaPage.fields);
 
   function handler(values) {
-    mutate.mutate(values);
-    console.log(values);
+    setStatus(null);
+    isSubmit(true);
+    return mutate.mutate(values);
   }
 
   return (
-    <div className="fixed left-0  right-0 top-0  p-4 font-dm-sans">
-      <form
-        className="flex flex-col space-y-6 border-2 border-line/50 p-12"
-        onSubmit={form.onSubmit((values) => handler(values))}
-      >
+    <div className="flex h-screen items-center justify-center p-4 font-dm-sans">
+      <form className="flex w-1/2 flex-col space-y-6 p-12" onSubmit={form.onSubmit((values) => handler(values))}>
+        <div className="flex flex-col space-y-1">
+          <h1 className="font-dm-display text-4xl font-bold">Login</h1>
+          <h5 className="font-dm-sans text-base  font-normal">Enter your credentials to access all of our features</h5>
+        </div>
         {fields.map((field) => (
           <FormWrapper key={field}>
             <label className="text-primary" htmlFor={field}>
@@ -49,16 +66,22 @@ const LoginPage = () => {
             </label>
             <input
               name={field}
-              className="w-full border-2 border-line/80 px-4 py-2 focus:border-primary focus:outline-none"
+              className={`w-full border-2 border-line/80 px-4 py-2 focus:border-primary focus:outline-none ${form.errors[field] ? "border-red-800" : ""}`}
               type={`${field === "password" ? "password" : "text"}`}
               placeholder={field}
               {...form.getInputProps(field)}
             />
+            <p className="text-sm text-red-800">{form.errors[field]}</p>
           </FormWrapper>
         ))}
-        <button type="submit" className="bg-black p-3 font-dm-display text-white">
-          Login
+        <button
+          type="submit"
+          className="border-2 border-transparent bg-black p-3 font-dm-display text-xl text-white  hover:border-black hover:bg-white hover:text-black"
+          disabled={submit}
+        >
+          {submit ? <Loader className="inline-flex h-5 w-5 animate-spin justify-center" /> : "Login"}
         </button>
+        {status ? <p className="text-red-800">{status}</p> : ""}
       </form>
     </div>
   );
@@ -69,3 +92,7 @@ export default LoginPage;
 function FormWrapper({ children }) {
   return <div className="flex flex-col space-y-3">{children}</div>;
 }
+
+FormWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
