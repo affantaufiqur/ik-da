ee1a0d974458024ab5a64ff7389e202b3c59fa92
Abdul Aziz Rahmawan <71802858+AzizRahmawan@users.noreply.github.com>
2024-02-20 11:29:15 +0700

    [BE]: Seeder Data (#23)
    * create model prisma database
    
    * update model prisma database
    
    * add auth routes
    
    * add user routes
    
    * add story routes
    
    * add chapter routes
    
    * update params.id in chapter routes
    
    * update params.id in story routes
    
    * update params.id in story routes
    
    * add genre routes
    
    * add bookmarks routes
    
    * add like book routes
    
    * import routes to index.js
    
    * prisma migrate -first migration
    
    * create prisma.js
    
    * wip user & rolle seed
    
    * update role and add user seed
    
    * add genre seed
    
    * add story seed
    
    * format code
    
    * update route list story
    
    * update routes detail story
    
    * update routes create story
    
    * update user & genre seed && update route list story
    
    * add route delete story && update route list story
    
    * add route update story
    
    * add new & update seed data

---
+++ server/seeder/bookmark-seed.js
@@ -0,0 +1,37 @@
+import prisma from "../app/prisma.js";
+
+async function main() {
+    await prisma.bookmark.deleteMany();
+
+    const users = await prisma.user.findMany();
+    const stories = await prisma.story.findMany();
+    
+    for (let i = 0; i < 1000; i++) {
+        const randomUser = users[Math.floor(Math.random() * users.length)];
+        const randomStory = stories[Math.floor(Math.random() * stories.length)];
+
+        const existingBookmark = await prisma.bookmark.findFirst({
+            where: {
+                user_id: randomUser.id,
+                story_id: randomStory.id,
+            },
+        });
+
+        if (!existingBookmark) {
+            await prisma.bookmark.create({
+                data: {
+                    user_id: randomUser.id,
+                    story_id: randomStory.id,
+                },
+            });
+        }
+    }
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
