f79f5b7b9d9c97705e18ce369faed7a2dac1df62
Amirul <muhamirul98@gmail.com>
2024-02-20 11:06:29 +0700

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { Typography } from "@material-tailwind/react";
 
 export function Footer() {
   return (
-    <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between xl:px-12 px-4">
       <Typography color="blue-gray" className="font-normal">
-        &copy; 2023 Material Tailwind
+        &copy; 2024 
       </Typography>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
         <li>
