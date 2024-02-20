aa4a29acf625cd92971a5b18f6f84f9fe338e1c4
Abdul Aziz Rahmawan <71802858+AzizRahmawan@users.noreply.github.com>
2024-02-20 14:46:53 +0700

    [BE}: Genre (#29)
    * create model prisma database
    
    * update model prisma database
    
    * add auth routes
    
    * add user routes
    
    * add story routes
    
    * add chapter routes
    
    * update params.id in chapter routes
    
    * update params.id in story routes
    
    * update params.id in story routes
    
    * add genre routes
    
    * add bookmarks routes
    
    * add like book routes
    
    * import routes to index.js
    
    * prisma migrate -first migration
    
    * create prisma.js
    
    * wip user & rolle seed
    
    * update role and add user seed
    
    * add genre seed
    
    * add story seed
    
    * format code
    
    * update route list story
    
    * update routes detail story
    
    * update routes create story
    
    * update user & genre seed && update route list story
    
    * add route delete story && update route list story
    
    * add route update story
    
    * update story & like routes
    
    * update bookmark routes
    
    * update bookmar, story routes & seeder
    
    * add CORS in routes story
    
    * add CORS in routes user
    
    * add CORS in routes upvote
    
    * add CORS in routes genre
    
    * add CORS in routes chapter
    
    * add CORS in routes bookmark
    
    * add CORS in routes auth
    
    * update routes genre
    
    * add pagination in genre routes
    
    * update direction stories

--- server/app/routes/genre-routes.js
+++ server/app/routes/genre-routes.js
@@ -1,15 +1,43 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/auth-middleware.js";
+import genreService from "../service/genre-service.js";
 
 const routes = Router();
 
 routes.get("/genres", async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ message: "Route get all genres" });
+    try {
+        const { page } = req.query;
+        const genres = await genreService.getAllGenre(Number(page));
+        if (genres.data.length < 1) {
+            return res.status(404).json({ message: "Genres not found" });
+        }
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(genres);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 routes.get("/genres/:genreId", async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ message: "Route get stories by genres" });
+    try {
+        const { genreId } = req.params;
+        const { page } = req.query;
+        const genre = await genreService.getGenreById(genreId, Number(page));
+        if (!genre) {
+            return res.status(404).json({ message: "Genre not found" });
+        }
+        if (genre.genre.stories.length < 1) {
+            return res.status(404).json({ message: "Stories not found" });
+        }
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(genre);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
+
+
 
 export default routes;
