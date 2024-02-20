b9759564257acd8e13b16087c4ebd114bf1a597a
AzizRahmawan <arder058@gmail.com>
2024-02-20 13:09:12 +0700

    add CORS in routes genre

--- server/app/routes/genre-routes.js
+++ server/app/routes/genre-routes.js
@@ -3,10 +3,12 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get("/genres", async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.json({ message: "Route get all genres" });
 });
 
 routes.get("/genres/:genreId", async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.json({ message: "Route get stories by genres" });
 });
 
