1dfc558338732a997374910e083fd0bd34170bf3
Julda Hifz <asad.julda@gmail.com>
2024-02-21 15:02:40 +0700

    Add WorkColumn to ProfilePage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import WorkColumn from "../components/WorkColumn";
 
 export default function ProfilePage() {
   const data = useLoaderData();
@@ -6,8 +7,9 @@ export default function ProfilePage() {
     <main className="px-4 md:px-12">
       <h1 className="font-dm-display text-2xl font-medium">{data.user.name}</h1>
       <section className="flex flex-col space-y-4">
-        {/* Work */}
-        <div></div>
+        <div>
+          <WorkColumn />
+        </div>
         {/* Bookmarks */}
         <div></div>
       </section>
