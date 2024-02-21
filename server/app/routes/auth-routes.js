1309401ae483b6fc9543b37161d9b5cc3a1d6682
Abdul Aziz Rahmawan <71802858+AzizRahmawan@users.noreply.github.com>
2024-02-21 11:57:34 +0700

    [BE] Genre Seeder (#36)
    * create model prisma database
    
    * update model prisma database
    
    * add auth routes
    
    * add user routes
    
    * add story routes
    
    * add chapter routes
    
    * update params.id in chapter routes
    
    * update params.id in story routes
    
    * update params.id in story routes
    
    * add genre routes
    
    * add bookmarks routes
    
    * add like book routes
    
    * import routes to index.js
    
    * prisma migrate -first migration
    
    * create prisma.js
    
    * wip user & rolle seed
    
    * update role and add user seed
    
    * add genre seed
    
    * add story seed
    
    * format code
    
    * update route list story
    
    * update routes detail story
    
    * update routes create story
    
    * update user & genre seed && update route list story
    
    * add route delete story && update route list story
    
    * add route update story
    
    * update story & like routes
    
    * update bookmark routes
    
    * update bookmar, story routes & seeder
    
    * add CORS in routes story
    
    * add CORS in routes user
    
    * add CORS in routes upvote
    
    * add CORS in routes genre
    
    * add CORS in routes chapter
    
    * add CORS in routes bookmark
    
    * add CORS in routes auth
    
    * update routes genre
    
    * add pagination in genre routes
    
    * update direction stories
    
    * update route profile
    
    * update genre seed & pagination story
    
    * update current user middleware

--- server/app/routes/auth-routes.js
+++ server/app/routes/auth-routes.js
@@ -4,6 +4,7 @@ import { loginSchema, registerSchema } from "../validation/auth-validation.js";
 import prisma from "../prisma.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const routes = Router();
 
@@ -62,7 +63,7 @@ routes.post("/register", async (req, res) => {
     }
 });
 
-routes.get("/current-user", (req, res) => {
+routes.get("/current-user", authMiddleware, (req, res) => {
     const bearer = req.headers.authorization;
     if (!bearer) {
         return res.status(401).json({ message: "Unauthorized" });
