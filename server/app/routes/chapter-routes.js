49f27b8b60167274bedb3deda38a0e0aa2bbbbed
affantaufiqur <affantaufiqur@gmail.com>
2024-02-22 12:36:24 +0700

    Chapter page

--- server/app/routes/chapter-routes.js
+++ server/app/routes/chapter-routes.js
@@ -15,27 +15,26 @@ routes.get("/stories/:storyId/chapters/:chapterId", async (req, res) => {
             return res.status(404).json({ message: "Chapter not found" });
         }
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.json(chapter);
+        return res.json(chapter);
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
-    res.json({ message: "Route get detail stories chapters" });
 });
 
 routes.post("/stories/:storyId/chapters", async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ message: "Route create stories chapters" });
+    return res.json({ message: "Route create stories chapters" });
 });
 
 routes.put("/stories/:storyId/chapters/:chapterId", async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ message: "Route update stories chapters" });
+    return res.json({ message: "Route update stories chapters" });
 });
 
 routes.delete("/stories/:storyId/chapters/:chapterId", async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ message: "Route delete stories chapters" });
+    return res.json({ message: "Route delete stories chapters" });
 });
 
 export default routes;
--- server/app/service/chapter-service.js
+++ server/app/service/chapter-service.js
@@ -1,19 +1,32 @@
 import prisma from "../prisma.js";
 
 class Chapter {
-    
     async getDetailChapter(storyId, chapterId) {
         const chapter = await prisma.chapter.findUnique({
             where: {
                 id: chapterId,
-                story_id: storyId
-            }
+                story_id: storyId,
+            },
+            include: {
+                story: {
+                    select: {
+                        chapters: {
+                            select: {
+                                id: true,
+                                created_at: true,
+                                title: true,
+                            },
+                            orderBy: {
+                                created_at: "desc",
+                            },
+                        },
+                    },
+                },
+            },
         });
         return chapter;
     }
-    async createChapter(chapter) {
-        
-    }
+    async createChapter(chapter) {}
 }
 
 export default new Chapter();
