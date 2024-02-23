b7adb76c9142a83cf646506cde6eee84e0cfd179
ikan <62418662+affantaufiqur@users.noreply.github.com>
2024-02-23 14:05:59 +0700

    [FE]: Add book (#50)
    * setting cors in backend
    
    * add tanstack query
    
    * configure tanstack query
    
    * add devtools
    
    * mutate login
    
    * backend: add cors
    
    * Login ui and fn
    
    * return user
    
    * ProfilePage
    
    * redirect when logout
    
    * navbar logout
    
    * Story page
    
    * give space
    
    * WIP: story page
    
    * add some progress bar
    
    * add search bar on chapters
    
    * story page yey
    
    * remove unused
    
    * Chapter page
    
    * Login page should be fixed
    
    * new add story route
    
    * fix register
    
    * WIP: add stroy
    
    * create story
    
    * schema update

--- server/app/service/genre-service.js
+++ server/app/service/genre-service.js
@@ -2,33 +2,8 @@ import prisma from "../prisma.js";
 
 class Genre {
     async getAllGenre(pageNumber) {
-        const page = pageNumber || 1;
-        const limit = 12;
-        const offset = (page - 1) * limit;
-
-        if (isNaN(Number(page)) && page) {
-            throw new Error("Invalid page number");
-        }
-
-        const genres = await prisma.genre.findMany({
-            take: limit,
-            skip: offset,
-        });
-        const totalCount = await prisma.genre.count();
-        const totalPage = Math.ceil(totalCount / limit);
-        const nextPage = page < totalPage ? page + 1 : null;
-        const prevPage = page > 1 ? page - 1 : null;
-
-        const listGenre = {
-            data: genres,
-            meta: {
-                total: totalCount,
-                total_page: totalPage,
-                prev_page: prevPage,
-                next_page: nextPage
-            }
-        }
-        return listGenre;
+        const genres = await prisma.genre.findMany({});
+        return genres;
     }
     async getGenreById(id, pageNumber) {
         const page = pageNumber || 1;
@@ -78,3 +53,4 @@ class Genre {
 }
 
 export default new Genre();
+
