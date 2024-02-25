7c0033c4e5e33873b8366edd7274c894a8ad327c
ikan <62418662+affantaufiqur@users.noreply.github.com>
2024-02-25 16:41:20 +0700

    [FE]: Write chapter (#53)
    * setting cors in backend
    
    * add tanstack query
    
    * configure tanstack query
    
    * add devtools
    
    * mutate login
    
    * backend: add cors
    
    * Login ui and fn
    
    * return user
    
    * ProfilePage
    
    * redirect when logout
    
    * navbar logout
    
    * Story page
    
    * give space
    
    * WIP: story page
    
    * add some progress bar
    
    * add search bar on chapters
    
    * story page yey
    
    * remove unused
    
    * Chapter page
    
    * Login page should be fixed
    
    * new add story route
    
    * fix register
    
    * WIP: add stroy
    
    * create story
    
    * schema update
    
    * setup write chapter page
    
    * setup lexical
    
    * add button to write new story
    
    * style button
    
    * install tiptap
    
    * WYSIWYG editor
    
    * make schema use json
    
    * validation
    
    * organize import
    
    * navigate to chapter after writing

---
+++ client/src/components/Editor.jsx
@@ -0,0 +1,101 @@
+import * as Toggle from "@radix-ui/react-toggle";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Bold, Italic } from "lucide-react";
+import PropTypes from "prop-types";
+import { useRef, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { fetchData } from "../shared/fetch.js";
+import { getTokenFromCookies } from "../shared/token.js";
+
+export default function Editor({ editable = true, content = "" }) {
+  const params = useParams();
+  const token = getTokenFromCookies();
+  const navigate = useNavigate();
+  const storyId = params.storyId;
+
+  const [validation, setValidation] = useState(false);
+  const [submit, setSubmitStatus] = useState(false);
+  const inputRef = useRef(null);
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content,
+    editable,
+  });
+  if (!editor) return null;
+
+  async function handleEditor() {
+    const content = editor?.getJSON();
+    const title = inputRef?.current?.value;
+
+    const data = {
+      title,
+      content,
+    };
+
+    const post = await fetchData(`stories/${storyId}/chapters`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (post.message === "Internal server error") {
+      return setSubmitStatus("Failed on creating new chapter");
+    }
+    return navigate(`/story/${storyId}/chapter/${post.chapter.id}`);
+  }
+
+  function trigger() {
+    setValidation(false);
+    const title = inputRef?.current?.value;
+    if (title.length < 1) {
+      return setValidation("Title cannot be empty");
+    }
+    return handleEditor();
+  }
+
+  return (
+    <main className="flex flex-col space-y-8">
+      <section className="flex flex-col space-y-2">
+        <label htmlFor="title">Chapter title</label>
+        <input
+          ref={inputRef}
+          className="w-full border-2 border-line px-3 py-2.5 focus:outline-none focus:ring-transparent lg:w-1/3"
+          name="title"
+        />
+        {validation && <p className="text-red-500">{validation}</p>}
+      </section>
+      <section className="flex flex-col space-y-2 border-2 border-line">
+        <div className="p-2">
+          <section className="flex w-1/3 flex-row space-x-1 border-[1px] border-line/20 p-1">
+            <Toggle.Root
+              onPressedChange={() => editor?.chain().focus().toggleBold().run()}
+              className={`p-2 ${editor?.isActive("bold") ? "bg-primary text-white" : ""}`}
+            >
+              <Bold className="h-4 w-4" />
+            </Toggle.Root>
+            <Toggle.Root
+              onPressedChange={() => editor?.chain().focus().toggleItalic().run()}
+              className={`p-2 ${editor?.isActive("italic") ? "bg-primary text-white" : ""}`}
+            >
+              <Italic className="h-4 w-4" />
+            </Toggle.Root>
+          </section>
+        </div>
+        <EditorContent editor={editor} className="min-h-24 px-3 py-2.5 focus:outline-none focus:ring-transparent" />
+      </section>
+      <button className="btn-primary" onClick={trigger}>
+        Publish
+      </button>
+      <Link to={`/story/sotryid/chapter/id`}>testing</Link>
+      {submit && <p className="text-red-500">{submit}</p>}
+    </main>
+  );
+}
+
+Editor.propTypes = {
+  editable: PropTypes.bool,
+  content: PropTypes.string,
+};
---
+++ client/src/pages/AddChapter.jsx
@@ -0,0 +1,9 @@
+import Editor from "../components/Editor";
+
+export default function WriteChapter() {
+  return (
+    <main className="px-4 md:mx-24">
+      <Editor />
+    </main>
+  );
+}
