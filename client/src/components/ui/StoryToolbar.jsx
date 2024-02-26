7f05ae05518988bf1b07ffb098e4115dac700770
ikan <62418662+affantaufiqur@users.noreply.github.com>
2024-02-26 23:46:44 +0700

    [FE]: Story page responsive UI (#69)
    * setting cors in backend
    
    * add tanstack query
    
    * configure tanstack query
    
    * add devtools
    
    * mutate login
    
    * backend: add cors
    
    * Login ui and fn
    
    * return user
    
    * ProfilePage
    
    * redirect when logout
    
    * navbar logout
    
    * Story page
    
    * give space
    
    * WIP: story page
    
    * add some progress bar
    
    * add search bar on chapters
    
    * story page yey
    
    * remove unused
    
    * Chapter page
    
    * Login page should be fixed
    
    * new add story route
    
    * fix register
    
    * WIP: add stroy
    
    * create story
    
    * schema update
    
    * setup write chapter page
    
    * setup lexical
    
    * merge
    
    * fix routing
    
    * WIP: Bookmar
    
    * revalidate page when bookmarked
    
    * upvote done
    
    * fix overflow
    
    * Modify overall ui

--- client/src/components/ui/StoryToolbar.jsx
+++ client/src/components/ui/StoryToolbar.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link, useLoaderData } from "react-router-dom";
 import { Menu as Dropdown, MenuHandler, MenuList } from "@material-tailwind/react";
-import { Bookmark, ChevronUp, MoreVertical } from "lucide-react";
+import { Plus, Bookmark, ChevronUp, MoreVertical } from "lucide-react";
 import { fetchData } from "../../shared/fetch.js";
 import { useParams, useNavigate } from "react-router-dom";
 import { getTokenFromCookies } from "../../shared/token.js";
@@ -50,13 +50,13 @@ export default function StoryToolbar({ isUser, upvote, bookmarkHandle, upvoteHan
   }
 
   return (
-    <section className="flex flex-row space-x-3 lg:space-x-1">
+    <section className="flex flex-row space-x-1 lg:space-x-2">
       {isUser ? (
         <Link
           to={"new-chapter"}
-          className="inline-flex h-10 items-center justify-center border-[1px] border-line/50 px-8 text-sm"
+          className="hidden h-10 w-10 items-center justify-center border-[1px] border-line/50 text-sm md:inline-flex"
         >
-          New chapter
+          <Plus className="h-4 w-4" />
         </Link>
       ) : null}
       <div
@@ -69,7 +69,7 @@ export default function StoryToolbar({ isUser, upvote, bookmarkHandle, upvoteHan
       </div>
       <div
         onClick={handleUpvote}
-        className={`group flex flex-row items-center justify-center space-x-4 border-[1px] border-line/50 px-6 transition-all duration-100 hover:bg-black ${pageData.isLiked ? "bg-black" : ""}`}
+        className={`group flex flex-row items-center justify-center space-x-4 border-[1px] border-line/50 px-3 transition-all duration-100 hover:bg-black ${pageData.isLiked ? "bg-black" : ""}`}
       >
         <h3
           className={`tansition-all cursor-pointer font-dm-sans text-sm font-medium text-primary duration-100 group-hover:text-white ${pageData.isLiked ? "text-white" : ""}`}
@@ -87,6 +87,12 @@ export default function StoryToolbar({ isUser, upvote, bookmarkHandle, upvoteHan
               <MoreVertical className="tansition-all h-4 w-4 text-primary duration-100 group-hover:cursor-pointer group-hover:text-white" />
             </MenuHandler>
             <MenuList className="decoration-none flex flex-col space-y-2 rounded-none font-dm-sans hover:border-none lg:w-[240px]">
+              <Link
+                to={"new-chapter"}
+                className="md:hiddedn p-1.5 ring-transparent hover:bg-gray-100 hover:ring-transparent "
+              >
+                New Chapter
+              </Link>
               <Link to={"edit"} className="p-1.5 ring-transparent hover:bg-gray-100 hover:ring-transparent">
                 Edit
               </Link>
