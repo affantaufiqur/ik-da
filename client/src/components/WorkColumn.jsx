371d63db42788ed72e74fb4d8b1ae118ebecc2f0
Affan <affantaufiqur@gmail.com>
2024-02-27 18:17:07 +0700

    modify work column

--- client/src/components/WorkColumn.jsx
+++ client/src/components/WorkColumn.jsx
@@ -2,6 +2,7 @@ import BookCard from "./BookCard";
 import { useFetch } from "../hooks/fetch-hooks";
 import { useLoaderData } from "react-router-dom";
 import TitleSection from "./ui/TitleSection.jsx";
+import Chip from "./ui/Chip.jsx";
 
 const WorkColumn = () => {
   const userData = useLoaderData();
@@ -18,7 +19,26 @@ const WorkColumn = () => {
       <section className="mt-4">
         <div className="grid grid-cols-3 gap-12 sm:grid-cols-6 lg:grid-cols-12">
           {getFourData.map((item) => (
-            <BookCard key={item.id} id={item.id} title={item.title} imgUrl={item.cover_img} chapter={"chapter 21"} />
+            <BookCard
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              imgUrl={item.cover_img}
+              chapter={"chapter 21"}
+              renderFn={() => (
+                <section className="flex flex-col space-y-3">
+                  <div className="flex flex-row flex-wrap gap-2 ">
+                    <Chip text={item?.author.name} />
+                    <Chip text={item?.genre.name} />
+                    <div className="bg-[#E2EFDE] p-1.5">
+                      <h4 className="inline-flex items-center justify-center px-3 font-dm-sans text-sm font-bold text-primary md:text-base">
+                        {new Intl.NumberFormat("en-US").format(item.upvote)} upvotes
+                      </h4>
+                    </div>
+                  </div>
+                </section>
+              )}
+            />
           ))}
         </div>
       </section>
